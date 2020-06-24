df8d25837e908e5034e2da691bc011c908064c56
jaumeortola <jaumeortola@gmail.com>
2020-06-24 11:36:45 +0200

    adjust prefixes

--- convert-metadix-dix.py
+++ convert-metadix-dix.py
@@ -60,16 +60,12 @@ for e in mainsection.iter(tag='e'):
     par = e.find('par')
     if par is None:
         continue
-    wordStr = word(e)
-    if len(wordStr)<5 or '<b/>' in wordStr or '_' in wordStr:
-        continue
     parname = par.get("n")
     for prefix in prefixes.keys():
         if parname.endswith("__"+prefix):
             new = ET.Element('par')
             prefixtoadd = prefixes[prefix]
-            
-            if wordStr.startswith("r"):
+            if word(e).startswith("r"):
                 prefixtoadd = prefixtoadd + ("_r")
             new.set('n', prefixtoadd)
             e.insert(0, new)
