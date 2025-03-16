8931233fb92a9e6fd51dece5fb1bc60844494b94
Marc Riera <marc.riera.irigoyen@gmail.com>
2025-03-16 12:59:47 +0100

    Update grep test

--- test/test-grep.sh
+++ test/test-grep.sh
@@ -6,24 +6,21 @@ echo "Grep tests"
 {
 for file in apertium-spa.spa.metadix
 do 
-    grep -H "<i> " $file
-    grep -H " </i>" $file
-    grep -H "<l> " $file
-    grep -H " </l>" $file
-    grep -H "<r> " $file
-    grep -H " </r>" $file
     grep -H -P "\xa0" $file # non-breaking space
-    grep -H "<b/><s" $file
-    grep -H "<b/><b/>" $file
-    grep -H "<b/><g>" $file
     grep -H -P "<i>[^<]+ [^<]+</i>" $file
     grep -H -P "<l>[^<]+ [^<]+</l>" $file
     grep -H -P "<r>[^<]+ [^<]+</r>" $file
+done
 
-    #Poden ser correctes
-    grep -H "<b/></l>" $file | sed -E '/n="cont/d'
-    grep -H "<b/></r>" $file | sed -E '/n="cont/d'
-    grep -H "<b/></i>" $file | sed -E '/n="cont/d' 
-    
+for file in $(mktemp)
+do
+    lt-expand apertium-spa.spa.metadix > $file
+    grep " :" $file
+    grep ": " $file
+    grep " <" $file
+    grep "  " $file
+    grep " #" $file
+    grep -P "^ " $file
+    rm $file
 done
 }
